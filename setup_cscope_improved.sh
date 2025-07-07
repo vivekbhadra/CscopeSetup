f496922608004731bc38711166b0dde52d6b5f25
Vivek Bhadra <vivek.bhadra@gmail.com>
2025-07-07 14:53:04 +0100

    Update setup_cscope_improved.sh

--- setup_cscope_improved.sh
+++ setup_cscope_improved.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-find -type d -path ./output -prune -type f -iname '*.c' -o -iname '*.cpp' -o -iname '*.h' -o -iname '*.hpp' -o -iname '*.cc' -o -iname '*.hh'  -type f > ./cscope.files
+
+# Generate a file list while excluding the output directory
+find . -type d -path ./output -prune -o -type f \( \
+    -iname '*.c' -o -iname '*.cpp' -o -iname '*.h' -o \
+    -iname '*.hpp' -o -iname '*.cc' -o -iname '*.hh' \
+\) > ./cscope.files
+
+# Build the Cscope database
 cscope -q -R -b -i ./cscope.files -f ./cscope.out
+
+# Generate the Ctags database while excluding noisy folders
 ctags --exclude=output --exclude=package --exclude=system -R .
-export CSCOPE_DB=./cscope.out
+
+# Export the Cscope environment variable for the current session
+export CSCOPE_DB="./cscope.out"
+
+echo "Cscope and Ctags databases have been successfully generated."
