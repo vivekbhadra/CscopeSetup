04849f0d0668ebe6c8e34083a8ee67b96c86c84a
Vivek Bhadra <vivek.bhadra@gmail.com>
2025-07-07 14:52:41 +0100

    Update setup_cscope.sh

--- setup_cscope.sh
+++ setup_cscope.sh
@@ -1,11 +1,22 @@
+#!/bin/bash
+
 CSCOPE_DIR=~/xyz
 
-if [ ! -d ${CSCOPE_DIR} ]; then
-	mkdir -p ${CSCOPE_DIR}
+# Create the directory if it doesn't exist
+if [ ! -d "${CSCOPE_DIR}" ]; then
+    mkdir -p "${CSCOPE_DIR}"
 fi
 
-find ${PWD} -type f -iname '*.c' -o -iname '*.cpp' -o -iname '*.h' -o -iname '*.hpp' -o -iname '*.cc' -o -iname '*.hh'  -type f  > ${CSCOPE_DIR}/cscope.files
-cscope -q -R -b -i ${CSCOPE_DIR}/cscope.files -f ${CSCOPE_DIR}/cscope.out
-CSCOPE_DB=${CSCOPE_DIR}/cscope.out
-export CSCOPE_DB
-echo "cscope setup done."
+# Generate the list of source files, properly grouping -o conditions
+find "${PWD}" -type f \( \
+    -iname '*.c' -o -iname '*.cpp' -o -iname '*.h' -o \
+    -iname '*.hpp' -o -iname '*.cc' -o -iname '*.hh' \
+\) > "${CSCOPE_DIR}/cscope.files"
+
+# Build the cscope database
+cscope -q -R -b -i "${CSCOPE_DIR}/cscope.files" -f "${CSCOPE_DIR}/cscope.out"
+
+# Export the database path
+export CSCOPE_DB="${CSCOPE_DIR}/cscope.out"
+
+echo "Cscope setup done."
